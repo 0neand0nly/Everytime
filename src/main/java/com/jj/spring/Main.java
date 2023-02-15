a00b216a8e5097c85336ae4fa697e11d1b9016e5
Jooyoung Jang <84753951+0neand0nly@users.noreply.github.com>
2023-02-15 15:18:28 +0900

    feat: test

--- src/main/java/com/jj/spring/Main.java
+++ src/main/java/com/jj/spring/Main.java
@@ -1,4 +1,6 @@
-package com.jj.spring;public class Main {
+package com.jj.spring;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
